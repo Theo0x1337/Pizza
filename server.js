f283b611d90061e71212b7b79d5f728d7b912ef6
theo bernardin <theo.bernardin@gmail.com>
2020-09-15 14:41:30 +0200

    ajout des logs

--- server.js
+++ server.js
@@ -5,8 +5,45 @@ const express = require('express');
 //Use chalk to add colours on the console
 const chalk = require('chalk');
 
+//The 404 middleware used when an incoming request
+//hits a wrong route
+const http404 = require('./middleware/route404');
+
+//Access the path 
+const path = require('path');
+
+//Used for logging
+const morgan = require("morgan");
+
+//Add more logging
+const {loggers, transports, format} = require("winston");
+
 //Create an application 
 const app = express();
+
+//Use the morgan logging 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+//Define the loggers for Winston
+loggers.add('infoLogger', {
+    level: 'info',
+    transports: [new transports.File({ filename: path.join(__dirname, 'logs/info.log')})],
+    format: format.printf((info) => {
+      let message = `${new Date(Date.now()).toUTCString()} | ${info.level.toUpperCase()}  | ${info.message}`
+      return message
+    })
+});
+
+loggers.add('errorLogger', {
+    level: 'error',
+    transports: [new transports.File({ filename: path.join(__dirname, 'logs/error.log')})],
+    format: format.printf((info) => {
+      let message = `${new Date(Date.now()).toUTCString()} | ${info.level.toUpperCase()}  | ${info.message}`
+      return message
+    })
+});
+
+const infoLogger = loggers.get('infoLogger');
 
 //Send back a raw message every time the server got an 
 //incoming request
@@ -15,6 +52,10 @@ app.get('/', (req, res) => {
     res.json({'msg':'You are reaching homepage'});
 
 });
+
+//When there is no route that caught the incoming request
+//use the 404 middleware
+app.use(http404.notFound);
 
 //Listen on the port 3000
 app.listen(3000);
